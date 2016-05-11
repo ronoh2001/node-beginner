c68a9d9bdbe1db1d0d0faebd8a914eee1bf637ca
ron oh <ronoh2001@gmail.com>
2016-05-10 22:48:57 -0700

    new repo for Node Begginer book code

---
+++ node-beginner/requestHandlers.js
@@ -0,0 +1,18 @@
+var exec = require('child_process').exec;
+
+function start() {
+  console.log('request handler "start" was called');
+  var content = 'empty';
+
+  exec('ls -lah', function (error, stdout, stderr) {
+    content = stdout;
+  });
+  return content;
+}
+
+function upload() {
+  console.log('request handler "upload" was called');
+}
+
+exports.start = start;
+exports.upload = upload;
